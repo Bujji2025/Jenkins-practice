8485f228fd06d913d64ead0fea5a21fa8ce1d795
Shamili <shamili.nyalakonda@gmail.com>
2026-03-12 17:56:08 +0530

    Fix App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Jenkins Pipeline!");
+        System.out.println("App is running...");
+        // Keep running forever
+        while (true) {
+            Thread.sleep(30000);
+            System.out.println("Still alive...");
+        }
     }
 
     public static int add(int a, int b) {
